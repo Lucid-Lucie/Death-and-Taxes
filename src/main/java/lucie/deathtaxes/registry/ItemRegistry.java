65fa60844d811dfa6ab31cfd542d3ffdb1b78d38
Lucie <lucie.developer@gmail.com>
2025-06-01 17:43:10 +0200

    Updated spawn egg to reflect current version.

--- src/main/java/lucie/deathtaxes/registry/ItemRegistry.java
+++ src/main/java/lucie/deathtaxes/registry/ItemRegistry.java
@@ -6,12 +6,13 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ItemRegistry
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.createItems(DeathTaxes.MODID);
 
-
-    public static final Holder<Item> SCAVENGER_SPAWN_EGG = ITEMS.register("scavenger_spawn_egg", location -> new SpawnEggItem(EntityTypeRegistry.SCAVENGER.value(), 0xFFFFFF, 0xFFFFFF, new Item.Properties()));
+    public static final DeferredHolder<Item, DeferredSpawnEggItem> SCAVENGER_SPAWN_EGG = ITEMS.register("scavenger_spawn_egg", () -> new DeferredSpawnEggItem(EntityTypeRegistry.SCAVENGER, 0x5f5954, 0x6c1d2e, new Item.Properties()));
 }
