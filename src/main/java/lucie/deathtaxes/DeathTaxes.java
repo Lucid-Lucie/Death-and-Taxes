bf25e1e37c3b2774dcfcf3a115433e24a70e37ef
Lucie <lucie.developer@gmail.com>
2025-08-05 11:19:31 +0200

    Rework started for version 2.0.0

--- src/main/java/lucie/deathtaxes/DeathTaxes.java
+++ src/main/java/lucie/deathtaxes/DeathTaxes.java
@@ -1,26 +1,9 @@
 package lucie.deathtaxes;
 
-import lucie.deathtaxes.registry.*;
-import net.minecraft.resources.ResourceLocation;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 
-@Mod(DeathTaxes.MODID)
+@Mod(DeathTaxes.MOD_ID)
 public class DeathTaxes
 {
-    public static final String MODID = "deathtaxes";
-
-    public DeathTaxes(IEventBus modBus)
-    {
-        AttachmentTypeRegistry.ATTACHMENT_TYPES.register(modBus);
-        ParticleTypeRegistry.PARTICLE_TYPES.register(modBus);
-        SoundEventRegistry.SOUND_EVENTS.register(modBus);
-        EntityTypeRegistry.ENTITY_TYPES.register(modBus);
-        ItemRegistry.ITEMS.register(modBus);
-    }
-
-    public static ResourceLocation withModNamespace(String path)
-    {
-        return ResourceLocation.fromNamespaceAndPath(DeathTaxes.MODID, path);
-    }
+    public static final String MOD_ID = "deathtaxes";
 }
